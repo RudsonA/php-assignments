e4cfd5c52349727d415eb7f8572dade1eae08fd0
RudsonA <rudson.augustin@yahoo.com>
2019-02-16 13:30:43 -0500

    foreach for index arrays

---
+++ php-practice/forEach.php
@@ -0,0 +1,13 @@
+
+<?php
+
+// Create an array of names 
+$names = ["Michael", "Judah", "Rueben", "Abraham", 'Jacob', "Israeli", "Adam"];
+
+//Print to the browser that each of them are biblical names. Use the p tag.
+foreach ($names as $name) {
+    echo $name . " is a biblical name" . "<br>";
+}
+
+
+?>

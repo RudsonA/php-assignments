47b22aace9067d6fb7cb6d7fb6d3b6fa99595145
RudsonA <rudson.augustin@yahoo.com>
2019-02-17 10:49:42 -0500

    exercise #1 complete.

---
+++ php-practice/arraysexe.php
@@ -0,0 +1,15 @@
+
+<!-- https://www.phpexercises.com/php-arrays-exercises.html -->
+
+<?php
+//PHP Arrays Ex. #1: Simple Array
+    //Write an array variable of weather conditions with the following values: rain, sunshine, clouds, hail, sleet, snow, wind. Using the array variable for all the weather conditions, echo the following statement to the browser:
+
+$weatherConditions = ["rain", "sunshine", "clouds", "hail", 'sleet', 'snow', 'wind'];
+
+// echo "We've seen all kinds of weather this month. At the beginning of the month, we had " . $weatherConditions[5] . 'and ' $weatherConditions[6] . '. Then came ' . $weatherConditions[1] . 'with a few ' . $weatherConditions[2] . ' and some ' . $weatherConditions[0] . ". At least we didn't get any" $weatherConditions[3] . " and " . $weatherConditions[4] . ".";
+
+echo "We've seen all kinds of weather this month. At the beginning of the month, we had $weatherConditions[5] and $weatherConditions[6]. Then came $weatherConditions[1] with a few $weatherConditions[2] and some $weatherConditions[0]. At least we didn't get any $weatherConditions[3] and $weatherConditions[4]";
+echo "<br>";
+?>
+
